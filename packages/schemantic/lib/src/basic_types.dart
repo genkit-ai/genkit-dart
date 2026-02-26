5b828628d65b0d56ad6a5275b8926350ea87ab83
Nate Bosch <nbosch@google.com>
2026-02-26 00:47:12 +0000

    Unused imports

--- packages/schemantic/lib/src/basic_types.dart
+++ packages/schemantic/lib/src/basic_types.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:convert';
-
 import 'package:json_schema_builder/json_schema_builder.dart' as jsb;
 import '../schemantic.dart';
 
