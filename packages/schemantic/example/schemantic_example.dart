d0dd74251e38f27402f4b2563c0778f72ce56966
Kevin Moore <kevmoo@google.com>
2026-02-26 13:56:49 -0800

    make sure to run schemantic builder - examples have built output Also cleanup lints ignores in the example

--- packages/schemantic/example/schemantic_example.dart
+++ packages/schemantic/example/schemantic_example.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// ignore_for_file: strict_top_level_inference, always_declare_return_types, type_annotate_public_apis
-
 import 'dart:convert';
 
 import 'package:schemantic/schemantic.dart';
@@ -26,7 +24,7 @@ abstract class $Address {
   String get city;
 
   @AnyOf([int, String])
-  get zipCode;
+  Object get zipCode;
 }
 
 /// Define a schema using the @Schematic annotation.
